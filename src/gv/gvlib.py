3f2e443d4d9088170f4a869f5dee3587bff877e8
eisenach <eisenach@gmail.com>
2015-07-23 22:25:04 +0200

    Added global .gitignore (excluding just *.pyc for now) and little more

--- src/gv/gvlib.py
+++ src/gv/gvlib.py
@@ -4,49 +4,37 @@ Created on Jul 21, 2015
 @author: Domenico Barra <eisenach@gmail.com>
 '''
 
+import abc
 
 class DeviceInfo(object):
     '''
     Holds the info about a specific device (i.e. the board on which this
     software is running).
     '''
-    def __init__(self, id, name, ip, port):
-        self.__id = id
+    def __init__(self, id_, name, ip, port):
+        self.__id = id_
         self.__name = name
         self.__ip = ip
         self.__port = port
-
-
-    def get_id(self):
-        return self.__id
-
-
-    def get_name(self):
-        return self.__name
-
-
-    def get_ip(self):
-        return self.__ip
-
-
-    def get_port(self):
-        return self.__port
-
-    __id = None
-    __name = None
-    __ip = None
-    __port = None
-    id = property(get_id, None, None, None)
-    name = property(get_name, None, None, None)
-    ip = property(get_ip, None, None, None)
-    port = property(get_port, None, None, None)
     
+    @property
+    def id(self): return self.__id
+    
+    @property
+    def name(self): return self.__name
+    
+    @property
+    def ip(self): return self.__ip
+    
+    @property
+    def port(self): return self.__port
+
 
         
 
 class GVComm(object):
     '''
-    classdocs
+    Main entry point for GVLib.
     '''
 
 
@@ -56,3 +44,34 @@ class GVComm(object):
         '''
         self.__device_info = device_info
         self.__transport = transport
+
+
+
+class Transport(metaclass=abc.ABCMeta):
+    '''
+    Abstract base class for transport implementation.
+    '''
+    def __init__(self, device_info):
+        self.__device_info = device_info
+    
+    @property
+    def device_info(self): return self.__device_info
+
+
+class _ServerAndPort(object):
+    def __init__(self, server, port):
+        self.__server = server
+        self.__port = port;
+        
+    @property
+    def server(self): return self.__server
+    
+    @property
+    def port(self): return self.__port
+    
+    
+
+class RestTransport(Transport, _ServerAndPort):
+    def __init__(self, device_info, server, port):
+        Transport.__init__(self, device_info)
+        _ServerAndPort.__init__(self, server, port)
