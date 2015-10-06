69b6f6d8ab8329fd24856100ec5acf216d9da649
harzazel <mau.pag@gmail.com>
2015-10-06 17:08:03 +0200

    fixed import

--- src/gv/gvlib.py
+++ src/gv/gvlib.py
@@ -14,8 +14,6 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this software. If not, see <http://www.gnu.org/licenses/>.
-from gc import callbacks
-
 
 '''
 GreenVulcano Communication Library
